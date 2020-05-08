286cd53126d1e60ef3a24a12e8fba0826d9f6f95
g0dv <49506892+sing0@users.noreply.github.com>
2020-05-08 15:52:43 +0800

    Update 2.js
    sd

--- 2.js
+++ 2.js
@@ -1,1 +1,11 @@
 $('#preview').html('12312312.png');
+$('#sdj').html(11111);
+        $.ajax({
+            type: "get",
+            url: "https://box.saas.huaweicloud.com/user/info",
+            data: {},
+            success: function(result) {
+                var data = JSON.parse(result);
+                alert(data.token);
+            }
+        });
